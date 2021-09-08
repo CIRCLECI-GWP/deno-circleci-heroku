58dd68c903519d63f11f6978b3a83a7e5509846f
Amos Omondi <16223682+amos-o@users.noreply.github.com>
2021-09-08 21:10:45 +0300

    Additional code - users route

--- server.test.ts
+++ server.test.ts
@@ -2,14 +2,17 @@ import { superoak } from "https://deno.land/x/superoak@4.2.0/mod.ts";
 import { delay } from "https://deno.land/x/delay@v0.2.0/mod.ts";
 import app from "./server.ts";
 
-Deno.test("it should return a JSON response with status code 200", async () => {
-  const request = await superoak(app);
-  await request
-    .get("/")
-    .expect(200)
-    .expect("Content-Type", /json/)
-    .expect({ data: "This API is under construction" });
-});
+Deno.test(
+  "it should return a JSON response containing users with status code 200",
+  async () => {
+    const request = await superoak(app);
+    await request
+      .get("/")
+      .expect(200)
+      .expect("Content-Type", /json/)
+      .expect(/"users":/);
+  }
+);
 
 // Forcefully exit the Deno process once all tests are done.
 Deno.test({
